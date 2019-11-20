7001d4d866df6c4a4c8b51915169f6ca1079cff3
Fabiano Traple <fabianotraple@gmail.com>
2019-11-20 13:29:01 -0300

    Update tests.

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -9,14 +9,50 @@ inline bool FileExist(const std::string& name) {
     return fileIn.good();
 }
 
-TEST(Test, Demo) {
-
+TEST(DigitalSignature, TestIfKeyFileWasCreated) {
+    // Arrange
     std::string publicPemFileName{"rsa-public.pem"};
     std::string privatePemFileName{"rsa-private.pem"};
-
     EncodeSignature encodeSign;
+    // Act
     encodeSign.CreateKeyPairFile(publicPemFileName, privatePemFileName);
+    // Assert
+    ASSERT_TRUE(FileExist(publicPemFileName));
+    ASSERT_TRUE(FileExist(privatePemFileName));
+}
 
-    EXPECT_TRUE(FileExist(publicPemFileName));
-    EXPECT_TRUE(FileExist(privatePemFileName));
+TEST(DigitalSignature, TestValidSignature) {
+    // Arrage
+    EncodeSignature encodeSign("rsa-private.pem");
+    DecodeSignature decodeSign("rsa-public.pem");
+    // Act
+    std::string message{"This is a message test."};
+    std::string signature = encodeSign.RSASign(message);
+    // Assert
+    ASSERT_TRUE(decodeSign.VerifySignature(message, signature));
+}
+
+TEST(DigitalSignature, TestInvalidSignature) {
+    // Arrage
+    EncodeSignature encodeSign("rsa-private.pem");
+    DecodeSignature decodeSign("rsa-public.pem");
+    // Act
+    std::string message{"This is a message test."};
+    std::string signature = encodeSign.RSASign(message);
+    message.append("change the message");
+    // Assert
+    ASSERT_FALSE(decodeSign.VerifySignature(message, signature));
+}
+
+TEST(DigitalSignature, Test_10_ValidSignature) {
+    // Arrage
+    EncodeSignature encodeSign("rsa-private.pem");
+    DecodeSignature decodeSign("rsa-public.pem");
+    // Act
+    for (int i = 0; i < 10; i++) {
+        std::string message = "This is the message test number: " + std::to_string(i);
+        std::string signature = encodeSign.RSASign(message);
+        // Assert
+        ASSERT_TRUE(decodeSign.VerifySignature(message, signature));
+    }
 }
